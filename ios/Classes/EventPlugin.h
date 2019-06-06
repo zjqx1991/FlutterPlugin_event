f8cfd4f2fa79e44bbff90ff62eb5feb83359b8fb
RevanWang <zjqx1991@163.com>
2019-06-06 15:57:59 +0800

    init

---
+++ ios/Classes/EventPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface EventPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/EventPlugin.m
@@ -0,0 +1,20 @@
+#import "EventPlugin.h"
+
+@implementation EventPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"event_plugin"
+            binaryMessenger:[registrar messenger]];
+  EventPlugin* instance = [[EventPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
